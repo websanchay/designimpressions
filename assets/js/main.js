6b9c404fd962b0c7eba1e4cffe68250dfdade836
websanchay <68428456+websanchay@users.noreply.github.com>
2021-11-22 09:51:14 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,4 +261,16 @@
 })()
 
 
-/**************************** ZOOM  *************************************** */
+/**************************** delete cookies  *************************************** */
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = www.thecreatives.in + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+deleteAllCookies();
